88de13ab30c1f11f4ada74e4b093009e384fbaa0
taalajgul <taalajgul736@gmail.com>
2025-12-06 18:53:55 +0300

    Загрузка проекта

---
+++ catalog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import*
+
+# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(MovieDirector)
+admin.site.register(MovieActor)
+admin.site.register(Review)
+admin.site.register(Collection)
